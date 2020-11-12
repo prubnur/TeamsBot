0f668239db5b38c425f923a68e3cdee3378970c7
Prabhnoor Saluja <isaluja1811@gmail.com>
2020-11-12 22:05:05 +0530

    waiting for elements

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
 from datetime import datetime, timedelta
 from threading import Timer
 
+from selenium.webdriver.support.wait import WebDriverWait
 
 def joinmeeting(url, duration, teamsid, teamspass):
     opt = Options()
@@ -24,7 +27,8 @@ def joinmeeting(url, duration, teamsid, teamspass):
 
     driver.set_page_load_timeout(10)
 
-    driver.get("https://teams.microsoft.com/_#/pre-join-calling/19:349461a7e103475a880deda07875ecdc@thread.tacv2") # DON'T CHANGE THIS LINK
+    #driver.get("https://teams.microsoft.com/_#/pre-join-calling/19:349461a7e103475a880deda07875ecdc@thread.tacv2") # DON'T CHANGE THIS LINK
+    driver.get(url)
     time.sleep(2)
     driver.find_element_by_name("loginfmt").send_keys(teamsid)
     driver.find_element_by_name("loginfmt").send_keys(Keys.ENTER)
@@ -33,9 +37,8 @@ def joinmeeting(url, duration, teamsid, teamspass):
     driver.find_element_by_id("i0118").send_keys(Keys.ENTER)
     time.sleep(1)
     driver.find_element_by_id("idSIButton9").click()
-    time.sleep(6)
-    driver.get(url)
-    time.sleep(6)
+    WebDriverWait(driver, 100).until(
+        EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Use the web app instead')]")))
 
     try:
         driver.find_element_by_xpath("//*[contains(text(), 'Use the web app instead')]").click()
@@ -43,6 +46,8 @@ def joinmeeting(url, duration, teamsid, teamspass):
     except:
         print("No prompt")
 
+    WebDriverWait(driver, 100).until(
+        EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Join')]")))
     driver.find_element_by_xpath("//*[contains(text(), 'Join')]").click()
     time.sleep(3)
     #driver.find_element_by_class_name("ts-toggle-button-container").click()
@@ -75,17 +80,17 @@ if not (userid and password):
     password = input("Enter your teams password: ")
 
 x=datetime.today()
-y = x.replace(day=x.day, hour=18, minute=15, second=0, microsecond=0) # + timedelta(days=1)
+y = x.replace(day=x.day, hour=8, minute=32, second=0, microsecond=0) + timedelta(days=1)
 delta_t=y-x
 
 secs=delta_t.total_seconds()
 print(secs)
 
 # Add the link to your meeting here
-link = "https://teams.microsoft.com/_#/scheduling-form/?eventId=AAMkADNiODUxOTlhLTQ4YmItNGIyYi05MDM0LTVjOGY5NDM2YTAwNgBGAAAAAACifGkwT19AQZ7GE2LvdHC5BwAXtDwmCk5PSaB7OKqdiU_VAAAAAAENAAAXtDwmCk5PSaB7OKqdiU_VAACLVI7EAAA%3D&conversationId=19:0dbac76ce6664198bef9f66a43e4ada8@thread.tacv2&opener=0&providerType=1&navCtx=channel"
+link = "https://teams.microsoft.com/_#/scheduling-form/?eventId=AAMkADQ0ZTFlZDEwLTVjNjAtNDFiNC1hZWY0LWNmYTYzNWJkYzgwYgBGAAAAAAAl3-u4iCSXQox5DMp39596BwDv7rJnnEaDSakCp-2zZ26IAAAAAAENAADv7rJnnEaDSakCp-2zZ26IAAAzzDqAAAA%3D&conversationId=19:7a04fe22e9364e77a601e575bf596edc@thread.tacv2&opener=0&providerType=1&navCtx=channel"
 
 # Add the Duration of your meeting in seconds
-dur = 7200
+dur = 5400
 
-t = Timer(secs, joinmeeting(link, dur, userid, password))
+t = Timer(secs, joinmeeting, [link, dur, userid, password])
 t.start()
